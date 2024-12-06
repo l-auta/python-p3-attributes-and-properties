d51f87b69877299748939c7d7b0927d25e19c900
lisa <lisaauta018@gmail.com>
2024-12-06 21:16:21 +0300

    Everything works but tests aint passing

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,36 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name = "None", breed = "Unknown"):
+        self.name = name
+        self.breed = breed
+
+    def _get_name(self):
+        return self._name
+
+    def _test_name_under_25(self, value):
+         if not isinstance(value, str) :
+            print("Name must be string between 1 and 25 characters.")
+         elif len(value) == 0 :
+             print("Name must be string between 1 and 25 characters.")
+         elif len(value) > 25 :
+             print("Name must be string between 1 and 25 characters.")
+         else:
+            self._name = value 
+
+    name = property(_get_name , _test_name_under_25)
+
+
+    @property
+    def breed(self):
+        return self._breed 
+
+    @breed.setter
+    def breed(self, value):
+         if value not in APPROVED_BREEDS:
+            print("Breed must be in list of approved breeds.")
+         else:
+            self._breed = value 
+
+   
+dog1 = Dog("" , "corgi")
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,35 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name, job):
+        self.name = name.title()
+        self.job = job
+
+    #for name
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name (self, value):
+        if not isinstance(value, str) or len(value) == 0 or len(value) > 25:
+            print("Name must be string between 1 and 25 characters")
+
+        else:
+            self._name = value
+
+    #for jobs
+    @property
+    def job(self):
+        return self._job
+
+    @job.setter
+    def job(self, value):
+        if not value in APPROVED_JOBS:
+            print("Job must be in list of approved jobs")
+
+        else:
+            self._job = value
+
+lisa = Person("lisa", "ITC")
+print(lisa.__dict__)
